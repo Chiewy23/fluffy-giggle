0a35e038fc50592032d81dff36e8efe47fd1eb8c
Christopher Hiew <christopherhiew@protonmail.com>
2022-06-13 08:34:04 +0100

    Created directory for Redux operations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './containers/App';
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import './index.css';
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,3 +17,4 @@ root.render(
 
 // ACTION
 // REDUCER
+// STORE

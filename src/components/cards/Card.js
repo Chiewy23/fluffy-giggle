adc9af1b28d1b6e6beefbefa988a991066c79838
Christopher Hiew <christopherhiew@protonmail.com>
2022-07-01 20:12:45 +0100

    Unit tests for Card component.

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -4,11 +4,11 @@ const Card = (props) => {
     const { name, email, id }  = props;
     
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>{ name }</h2>
-                <p>{ email }</p>
+                <h2 data-testid="name">{ name }</h2>
+                <p data-testid="email">{ email }</p>
             </div>
         </div>
     );
--- src/tests/Card.test.js
+++ src/tests/Card.test.js
@@ -1,5 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import Card from "../components/cards/Card";
 
 describe("When mounting the Card component", () => {
+    const stubProps = {
+        name: "Charlie",
+        email: "charlie@chaplin.com",
+        id: "1234"
+    };
 
+    it("should render the name props", () => {
+        render(<Card key={stubProps.id} name={stubProps.name} email={stubProps.email} id={stubProps.id} />);
+
+        expect(screen.getByTestId("name").textContent).toBe("Charlie");
+    });
+
+    it("should render the email props", () => {
+        render(<Card key={stubProps.id} name={stubProps.name} email={stubProps.email} id={stubProps.id} />);
+
+        expect(screen.getByTestId("email").textContent).toBe("charlie@chaplin.com");
+    });
 });

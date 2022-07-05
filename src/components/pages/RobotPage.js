9fb665a4a20016e0207c730349d8576174605b3e
Christopher Hiew <christopherhiew@protonmail.com>
2022-07-05 09:50:46 +0100

    Initial start to RobotPage.

---
+++ src/components/pages/RobotPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RobotPage = (props) => {
+    return <h1> This is a page...</h1>
+}
+
+export default RobotPage;
--- src/containers/App.js
+++ src/containers/App.js
@@ -8,11 +8,11 @@ import SearchBox from "../components/search/SearchBox";
 import Scroll from "../components/scroll/Scroll";
 import ErrorBoundary from "../components/error-handling/ErrorBoundary";
 import Header from "../components/header/Header";
+import RobotPage from '../components/pages/RobotPage';
 
 /*
 TO-DO:
 - Code splitting.
-- Unit tests.
 - Clickable robots --> Redirects to page for robot.
  */
 

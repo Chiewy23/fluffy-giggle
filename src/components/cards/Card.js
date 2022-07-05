a409bbbb15d3a9416960505f773975bb1f9a4776
Christopher Hiew <christopherhiew@protonmail.com>
2022-07-05 14:03:31 +0100

    Added onClick callback for Card component.

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Card = (props) => {
-    const { name, email, id }  = props;
+    const { name, email, id, onClick }  = props;
     
     return (
-        <div className="card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div onClick={() => onClick() } className="card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2 data-testid="name">{ name }</h2>
--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -2,8 +2,13 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
+
+    const onClick = (name) => {
+        console.log(`This is Robot ${name}`);
+    }
+
     return robots.map(robot => {
-        return <Card key={robot.id} name={robot.name} email={robot.email} id={robot.id} />
+        return <Card onClick={ () => onClick(robot.name) } key={robot.id} name={robot.name} email={robot.email} id={robot.id} />
     });
 }
 

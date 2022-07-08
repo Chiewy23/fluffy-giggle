c6fbffbe6c7ebd84f64e16a4c1db855dd347378f
Christopher Hiew <christopherhiew@protonmail.com>
2022-07-08 08:16:48 +0100

    Created instructions for GitHub pages deployment.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { Component } from 'react';
 import Home from "../components/home/Home";
 
+/**
+ * **** TO-DO ****
+ *  - Update icons.
+ *  - Implement React Router.
+ */
 
 class App extends Component {
     render() {

60d49d5658813f6c1c80d3d9b2b3db3e9664f9a1
Christopher Hiew <christopherhiew@protonmail.com>
2022-07-09 14:24:23 +0100

    Minro bug fix.
    Ensure dimensions for card size are passed in correctly through to the API.

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     
     return (
         <div onClick={ () => onClick(props) } className="custom-card card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
+            <img alt="robot" src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2 data-testid="name">{ name }</h2>
                 <p data-testid="email">{ email }</p>

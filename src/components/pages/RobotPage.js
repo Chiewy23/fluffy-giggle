c11f7a7b98f19037853d4052afa42da95a3bfcd2
Christopher Hiew <christopherhiew@protonmail.com>
2022-08-05 20:21:16 +0100

    Included header in robot page

--- src/components/pages/RobotPage.js
+++ src/components/pages/RobotPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DefaultPage from "./DefaultPage";
 import BasicButton from "../basic-button/BasicButton";
+import Header from "../header/Header";
 import "./robot-page.css";
 
 import { useLocation, useNavigate } from "react-router-dom";
@@ -18,21 +19,24 @@ const RobotPage = () => {
     }
 
     return (
-        <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
-            <div className="tc">
-                <img alt="robot" src={ `https://robohash.org/${state.id}?200x200` } />
-                <h1 className="f4">{ state.name }</h1>
-                <p>{ state.email }</p>
-                <hr className="mw3 bb bw1 b--black-10"/>
-            </div>
-            <p className="tc lh-copy measure center f6 black-70">
-                { state.text }
-            </p>
+        <React.Fragment>
+            <Header />
+            <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+                <div className="tc">
+                    <img alt="robot" src={ `https://robohash.org/${state.id}?200x200` } />
+                    <h1 className="f4">{ state.name }</h1>
+                    <p>{ state.email }</p>
+                    <hr className="mw3 bb bw1 b--black-10"/>
+                </div>
+                <p className="tc lh-copy measure center f6 black-70">
+                    { state.text }
+                </p>
 
-            <div className="robot-button">
-                <BasicButton onClick={ redirectToHomePage } text="Home" />
-            </div>
-        </article>
+                <div className="robot-button">
+                    <BasicButton onClick={ redirectToHomePage } text="Home" />
+                </div>
+            </article>
+        </React.Fragment>
     );
 }
 

b51858043e79503e42663b5266bbd937891b27fd
Christopher Hiew <christopherhiew@protonmail.com>
2022-08-23 15:12:45 +0100

    Converted Header to tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 class Header extends Component {
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
+    shouldComponentUpdate() {
         return false;
     }
 

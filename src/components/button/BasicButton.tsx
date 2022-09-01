d999c6c0ecc747324c79ab8ed0188138194267f6
Christopher Hiew <christopherhiew@protonmail.com>
2022-09-01 15:20:35 +0100

    Converted BasicButton to tsx.

--- src/components/button/BasicButton.tsx
+++ src/components/button/BasicButton.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-const BasicButton = ({ onClick, text }) => {
+interface IButton {
+    onClick: Function;
+    text: string;
+}
+
+const BasicButton = ({ onClick, text } : IButton) => {
     return (
         <button className="f6 br3 link dim ph3 pv2 mb2 dib white bg-dark-gray" 
                 onClick={ () => onClick() }

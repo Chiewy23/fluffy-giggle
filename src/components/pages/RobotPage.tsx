21346df5033fc195d6452321cb241839d861afec
Christopher Hiew <christopherhiew@protonmail.com>
2022-09-03 11:36:54 +0100

    Converted RobotPage to tsx.

--- src/components/pages/RobotPage.tsx
+++ src/components/pages/RobotPage.tsx
@@ -6,30 +6,40 @@ import "./robot-page.css";
 
 import { useLocation, useNavigate } from "react-router-dom";
 
+interface IStateRobotPage {
+    name: string;
+    email: string;
+    text: string;
+    id: string;
+}
+
 const RobotPage = () => {
-    const { state }  = useLocation();
+    const location = useLocation();
     const navigate = useNavigate();
 
     const redirectToHomePage = () => {
         navigate(`/fluffy-giggle`);
     }
 
-    if (state === null) {
-        return <DefaultPage onClick={ redirectToHomePage } />
+    if (location.state === null) {
+        return <DefaultPage onClick={ redirectToHomePage } text="Oops! We don't recognise that URL." />
     }
+
+    const { name, email, text, id }  = location.state as IStateRobotPage;
+    
 
     return (
         <React.Fragment>
             <Header />
             <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
                 <div className="tc">
-                    <img alt="robot" src={ `https://robohash.org/${state.id}?200x200` } />
-                    <h1 className="f4">{ state.name }</h1>
-                    <p>{ state.email }</p>
+                    <img alt="robot" src={ `https://robohash.org/${id}?200x200` } />
+                    <h1 className="f4">{ name }</h1>
+                    <p>{ email }</p>
                     <hr className="mw3 bb bw1 b--black-10"/>
                 </div>
                 <p className="tc lh-copy measure center f6 black-70">
-                    { state.text }
+                    { text }
                 </p>
 
                 <div className="robot-button">
